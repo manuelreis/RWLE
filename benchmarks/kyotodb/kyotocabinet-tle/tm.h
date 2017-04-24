87d9673cb01677ad9105da1ef7e3b8d4c540f632
Manuel Reis <manuel.barreto.reis@gmail.com>
2017-04-24 15:15:14 +0100

    Replaces Power8 with Intel codes.

--- benchmarks/kyotodb/kyotocabinet-tle/tm.h
+++ benchmarks/kyotodb/kyotocabinet-tle/tm.h
@@ -24,6 +24,9 @@
 #  define P_MEMORY_SHUTDOWN()           /* nothing */
 
 #  include <assert.h>
+#  include "memory.h"
+#  include "thread.h"
+#  include "types.h"
 #  include <math.h>
 
 #  define TM_ARG                        /* nothing */
@@ -54,79 +57,24 @@
 #    define local_thread_d()         thread_getId()
 #endif
 
-#include <htmxlintrin.h>
-
-# define CACHE_LINE_SIZE 128
-
-typedef struct padded_statistics {
-    unsigned long tle_commits;
-    unsigned long gl_commits;
-    unsigned long conflicts;
-    unsigned long self;
-    unsigned long trans;
-    unsigned long nontrans;
-    unsigned long capacity;
-    unsigned long other;
-    unsigned long user;
-    unsigned long persistent;
-    char suffixPadding[CACHE_LINE_SIZE];
-} __attribute__((aligned(CACHE_LINE_SIZE))) padded_statistics_t;
-
-extern __attribute__((aligned(CACHE_LINE_SIZE))) padded_statistics_t stats_array[];
+#  include <immintrin.h>
+#  include <rtmintrin.h>
 
 extern __thread int local_thread_id;
 
-extern __inline long
-__attribute__ ((__gnu_inline__, __always_inline__, __artificial__))
-__TM_is_self_conflict(void* const TM_buff)
-{ 
-  texasr_t texasr = __builtin_get_texasr ();
-  return _TEXASR_SELF_INDUCED_CONFLICT (texasr);
-}
-
-extern __inline long
-__attribute__ ((__gnu_inline__, __always_inline__, __artificial__))
-__TM_is_trans_conflict(void* const TM_buff)
-{ 
-  texasr_t texasr = __builtin_get_texasr ();
-  return _TEXASR_TRANSACTION_CONFLICT (texasr);
-}
-
-extern __inline long
-__attribute__ ((__gnu_inline__, __always_inline__, __artificial__))
-__TM_is_nontrans_conflict(void* const TM_buff)
-{ 
-  texasr_t texasr = __builtin_get_texasr ();
-  return _TEXASR_NON_TRANSACTIONAL_CONFLICT (texasr);
-}
-
 #  define TM_STARTUP(numThread, bId)
 #  define TM_SHUTDOWN() { \
-    unsigned long tle_commits = 0; \
-    unsigned long gl_commits = 0; \
-    unsigned long conflicts = 0; \
-    unsigned long self = 0; \
-    unsigned long trans = 0; \
-    unsigned long nontrans = 0; \
-    unsigned long capacity = 0; \
-    unsigned long user = 0; \
-    unsigned long persistent = 0; \
-    unsigned long other = 0; \
-    int i = 0; \
-    for (; i < 80; i++) { \
-       if (stats_array[i].tle_commits+stats_array[i].gl_commits == 0) { break; } \
-        tle_commits += stats_array[i].tle_commits; \
-        gl_commits += stats_array[i].gl_commits; \
-        conflicts += stats_array[i].conflicts; \
-        self += stats_array[i].self; \
-        trans += stats_array[i].trans; \
-        nontrans += stats_array[i].nontrans; \
-        capacity += stats_array[i].capacity; \
-        user += stats_array[i].user; \
-        persistent += stats_array[i].persistent; \ 
-        other += stats_array[i].other; \
-    } \
-    printf("Total commits: %lu\n\tHTM commits: %lu\n\tGL commits: %lu\nTotal aborts: %lu\n\tHTM conflict aborts: %lu\n\t\tHTM trans conflicts: %lu\n\t\tHTM non-trans conflicts: %lu\n\t\tHTM self conflicts: %lu\n\tHTM capacity aborts: %lu\n\tHTM persistent aborts: %lu\n\tHTM user aborts: %lu\n\tHTM other aborts: %lu\n", tle_commits+gl_commits,tle_commits, gl_commits, conflicts+capacity+user+persistent+other,conflicts,trans,nontrans,self,capacity,persistent,user,other); \
+        unsigned long commits = 0; \
+        unsigned long aborts = 0; \
+        unsigned long commits_with_lock = 0; \
+        int i = 0; \
+        for (; i < 128; i++) { \
+            if (statistics_array[i].commits + statistics_array[i].commits_with_lock == 0) { break; } \
+                commits += statistics_array[i].commits; \
+                aborts += statistics_array[i].aborts; \
+                commits_with_lock += statistics_array[i].commits_with_lock; \
+            } \
+        printf("TM commits: %lu\nTotal aborts: %lu\nTotal Lock Commits: %lu\n", commits, aborts, commits_with_lock); \
 }
 
 #  define TM_THREAD_ENTER()
@@ -137,46 +85,28 @@ __TM_is_nontrans_conflict(void* const TM_buff)
 # define SPEND_BUDGET(b)	if(RETRY_POLICY == 0) (*b)=0; else if (RETRY_POLICY == 2) (*b)=(*b)/2; else (*b)=--(*b);
 
 # define TM_BEGIN(mutex) { \
+        int tries = HTM_RETRIES; \
         while (1) { \
             while (IS_LOCKED(*mutex)) { \
-                    __asm volatile ("" : : : "memory"); \
+                __asm__ ( "pause;"); \
             } \
-            TM_buff_type TM_buff; \
-            unsigned char status = __TM_begin(&TM_buff); \
-            if (status == _HTM_TBEGIN_STARTED) { \
+            unsigned int status = _xbegin(); \
+            if (status == _XBEGIN_STARTED) { \
             	if (IS_LOCKED(*mutex)) { \
-            		__TM_abort(); \
+                    _xabort(30); \
             	} \
             	break; \
             } \
-	    else{ \
-                if(__TM_is_failure_persistent(&TM_buff)){ \
-                         SPEND_BUDGET(&htm_budget); \
-                         stats_array[local_thread_id].persistent++; \
-                } \
-                if(__TM_is_conflict(&TM_buff)){ \
-                        stats_array[local_thread_id].conflicts++; \
-                        if(__TM_is_self_conflict(&TM_buff)) {stats_array[local_thread_id].self++; }\
-                        else if(__TM_is_trans_conflict(&TM_buff)) stats_array[local_thread_id].trans++; \
-                        else if(__TM_is_nontrans_conflict(&TM_buff)) stats_array[local_thread_id].nontrans++; \
-                        htm_budget--; \
-                } \
-                else if (__TM_is_user_abort(&TM_buff)) { \
-                        stats_array[local_thread_id].user++; \
-                        htm_budget--; \
-                } \
-                else if(__TM_is_footprint_exceeded(&TM_buff)){ \
-                        stats_array[local_thread_id].capacity++; \
-                        htm_budget--; \
-                } \
-                else{ \
-                        stats_array[local_thread_id].other++; \
-                        htm_budget--; \
-                } \
+            else if (status == _XABORT_CAPACITY) { \
+                SPEND_BUDGET(&tries); \
             } \
-            if (htm_budget <= 0) {   \
+            else { \
+                tries--; \
+            } \
+            statistics_array[local_thread_id].aborts++; \
+            if (tries <= 0) {   \
             	while (::pthread_mutex_lock(mutex) != 0) { \
-                    __asm volatile ("" : : : "memory"); \
+                        __asm__ ("pause;"); \
                 } \
                 break; \
             } \
@@ -185,20 +115,20 @@ __TM_is_nontrans_conflict(void* const TM_buff)
 
 
 # define TM_END(mutex){ \
-    if (htm_budget > 0) { \
-        __TM_end(); \
-        stats_array[local_thread_id].tle_commits++; \
+    if (tries > 0) { \
+        _xend();
+        statistics_array[local_thread_id].commits++; \
     } else {    \
         ::pthread_mutex_unlock(mutex); \
-        stats_array[local_thread_id].gl_commits++; \
+        statistics_array[local_thread_id].commits_with_lock++; \
     } \
 };
 
 #    define TM_BEGIN_RO()                 TM_BEGIN(0)
-#    define TM_RESTART()                  __TM_abort();
+#    define TM_RESTART()                  _xabort(0xab);;
 #    define TM_EARLY_RELEASE(var)
 
-# define FAST_PATH_RESTART() __TM_abort();
+# define FAST_PATH_RESTART() _xabort(0xab);
 # define FAST_PATH_SHARED_READ(var) (var)
 # define FAST_PATH_SHARED_READ_P(var) (var)
 # define FAST_PATH_SHARED_READ_D(var) (var)
